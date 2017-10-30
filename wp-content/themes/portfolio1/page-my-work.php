56ec994ddf44b57772419ffad19023e087b9d0a9
Nikoleta Vlaskovic <Nikoleta Vlaskovic>
2017-10-30 19:45:42 +0100

    Categories, posts

--- wp-content/themes/portfolio1/page-my-work.php
+++ wp-content/themes/portfolio1/page-my-work.php
@@ -1,72 +1,58 @@
-<?php 
- // Template Name: Portfolio expose Template
+<?php
+/* Template Name: Portfolio Custom Template */ 
 get_header();
-the_post();
+the_post()
 ?>
-<!-- <div class="container">
-      <div class="row">
-          <div class="col-lg-6">
-            <?php the_content(); ?>
-          </div>
-      </div>
- </div> -->
-
 
 <!-- Page Content -->
-    <div class="container">
+<div class="container">
 
-      <!-- Page Heading/Breadcrumbs -->
-      <h1 class="mt-4 mb-3">Services
-        <small>Subheading</small>
-      </h1>
+  <!-- Page Heading/Breadcrumbs -->
+  <h1 class="mt-4 mb-3">Services
+    <small>Subheading</small>
+  </h1>
 
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item">
-          <a href="index.html">Home</a>
-        </li>
-        <li class="breadcrumb-item active">Services</li>
-      </ol>
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item">
+      <a href="index.html">Home</a>
+    </li>
+    <li class="breadcrumb-item active">Services</li>
+  </ol>
 
-      <!-- Image Header -->
-      <img class="img-fluid rounded mb-4" src="http://placehold.it/1200x300" alt="">
+  <!-- Image Header -->
+  <img class="img-fluid rounded mb-4" src="http://placehold.it/1200x300" alt="">
 
-      <!-- Marketing Icons Section -->
-      <div class="row">
+  <!-- Marketing Icons Section -->
+  <div class="row">
 
-      	<?php 
+    <?php
+    $categories = get_categories();
+    foreach ($categories as $category ) :
+      // var_dump($category);
+    ?>
 
-      		$query = new WP_Query([
-      			'post_type' => 'post',
-
-      		]);
-
-      		while($query->have_posts()):
-
-      			$query->the_post();
-
-      			?>
-      
-
-        <div class="col-lg-4 mb-4">
-          <div class="card h-100">
-            <h4 class="card-header"><?=the_title()?></h4>
-            <div class="card-body">
-              <p class="card-text"><?=excerpt(get_the_content())?></p>
-            </div>
-            <div class="card-footer">
-              <a href="<?=get_permalink()?>" class="btn btn-primary">learn more</a>
-            </div>
+      <div class="col-lg-4 mb-4">
+        <div class="card h-100">
+          <h4 class="card-header"> <?= $category->name ?> </h4>
+          <div class="card-body">
+            <p class="card-text"><?= $category->description ?></p>
+          </div>
+          <div class="card-footer">
+            <a href="<?= get_category_link($category->cat_ID); ?>" class="btn btn-primary">Learn More</a>
           </div>
         </div>
-
-        <?php 
-
-        	endwhile;
-        	wp_reset_postdata();
-
-        ?>
-       
-       
       </div>
-      <!-- /.row -->
+
+    <?php
+    endforeach;
+    wp_reset_postdata();
+    ?>
+
+    
+  </div>
+  <!-- /.row -->
+
+</div>
+<!-- /.container -->
+
 <?php get_footer(); ?>
